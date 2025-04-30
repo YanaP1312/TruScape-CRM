abe1fdb1883157759cb46e92050110c755b8f1cc
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-04-30 14:03:12 +0300

    add pages for company&company[id]

--- src/app/companies/[id]/page.tsx
+++ src/app/companies/[id]/page.tsx
@@ -1,11 +1,13 @@
+import Header from '@/app/components/header';
+
 export interface PageProps {
   params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
   return (
-    <main>
-      <h1 className="text-xl">Companies page</h1>
-    </main>
+    <>
+      <Header>Company ({params.id})</Header>
+    </>
   );
 }
--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,9 +1,33 @@
+import AddCompanyButton from '../components/add-company-button';
+import CompanyRow from '../components/company-row';
+import CompanyTable from '../components/company-table';
+import Header from '../components/header';
+import SearchInput from '../components/search-input';
+import { Status } from '../components/statusLabel';
+import Toolbar from '../components/toolbar';
+
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <main>
-      <h1 className="text-xl">Companies page</h1>
-    </main>
+    <>
+      <Header>Companies</Header>
+      <main>
+        <Toolbar action={<AddCompanyButton />}>
+          <SearchInput />
+        </Toolbar>
+        <CompanyTable>
+          <CompanyRow
+            id={1}
+            category="Products"
+            company="Costco"
+            status={Status.Pending}
+            promotion={true}
+            country="USA"
+            joinedDate="04.30.2025"
+          />
+        </CompanyTable>
+      </main>
+    </>
   );
 }

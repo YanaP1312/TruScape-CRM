0b54f6cda43cf02aef4f4bff7fb5163e6b673015
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-05-01 14:21:23 +0300

    change page component

--- src/app/companies/[id]/page.tsx
+++ src/app/companies/[id]/page.tsx
@@ -1,11 +1,7 @@
 import Header from '@/app/components/header';
 import React from 'react';
 
-export interface PageProps {
-  params: { id: string };
-}
-
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: { params: { id: string } }) {
   return (
     <>
       <Header>Company ({params.id})</Header>

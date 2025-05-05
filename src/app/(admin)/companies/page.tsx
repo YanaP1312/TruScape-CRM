18765cd97a709168ef29771990e8197c84c5c889
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-05-05 18:25:01 +0300

    add part for companies

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,22 +1,25 @@
-import CompanyRow from '../../components/company-row';
+import getQueryClient from '@/lib/utils/getQueryClient';
 import CompanyTable from '../../components/company-table';
-import { Status } from '../../components/status-label';
 import React from 'react';
+import { getCompanies } from '@/lib/api';
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 
 export interface PageProps {}
 
-export default function Page({}: PageProps) {
+export default async function Page({}: PageProps) {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['companies'],
+    queryFn: () => getCompanies({ cache: 'no-store' }),
+    staleTime: 10 * 1000,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <CompanyTable>
-      <CompanyRow
-        id={1}
-        category="Products"
-        company="Costco"
-        status={Status.Pending}
-        promotion={true}
-        country="USA"
-        joinedDate="04.30.2025"
-      />
-    </CompanyTable>
+    <HydrationBoundary state={dehydratedState}>
+      <CompanyTable />
+    </HydrationBoundary>
   );
 }
--- src/app/components/company-table.tsx
+++ src/app/components/company-table.tsx
@@ -1,6 +1,10 @@
-export interface CompanyTableProps {
-  children?: React.ReactNode;
-}
+'use client';
+
+import { getCompanies } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
+import CompanyRow from './company-row';
+
+export interface CompanyTableProps {}
 
 const headers = [
   'Category',
@@ -11,7 +15,13 @@ const headers = [
   'Joined date',
 ];
 
-export default function CompanyTable({ children }: CompanyTableProps) {
+export default function CompanyTable({}: CompanyTableProps) {
+  const { data } = useQuery({
+    queryKey: ['companies'],
+    queryFn: () => getCompanies(),
+    staleTime: 10 * 1000,
+  });
+
   return (
     <div className="py-8 px-10 bg-gray-100">
       <table className="table-auto w-full border-separate border-spacing-y-2">
@@ -24,7 +34,11 @@ export default function CompanyTable({ children }: CompanyTableProps) {
             ))}
           </tr>
         </thead>
-        <tbody>{children}</tbody>
+        <tbody>
+          {data?.map((company) => (
+            <CompanyRow key={company.id} company={company} />
+          ))}
+        </tbody>
       </table>
     </div>
   );
